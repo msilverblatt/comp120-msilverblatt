5625095ca9585c9119d45c07e7e19586e068d2d9
msilverblatt <msilverblatt@gmail.com>
2013-09-22 15:47:19 -0400

    ruby code to create database

---
+++ labs/mycouch.rb
@@ -0,0 +1,9 @@
+require ('couchdb');
+
+server = CouchDB::Server.new "67.23.79.113", 5984
+database = CouchDB::Database.new server, "msilverblatt"
+database.delete_if_exists!
+database.create_if_missing!
+
+document_one = CouchDB::Document.new database, "_id" => "doc_0", "category" => "one"
+document_one.save

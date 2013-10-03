e742721560afa8cf708c3b9fc6d97ab94f4a4298
msilverblatt <msilverblatt@gmail.com>
2013-10-03 02:35:13 -0400

    start a2

---
+++ a2/messagehub/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Messagehub::Application.config.secret_key_base = 'dfcef9b1527186780afb31bee30c0ff9b151c2865c8125b8e36cf5165e0e13fadc85e0985e1bc6fe23b634ce4f7fad6d627d38b08c5dd8c28fd50d8759a6ad81'

6a589f4ef03220afd88a0829cedae4ae55272bc6
msilverblatt <msilverblatt@gmail.com>
2013-10-06 17:15:43 -0400

    didn't break dbs, nice.

--- a2/messagehub/app/controllers/messages_controller.rb
+++ a2/messagehub/app/controllers/messages_controller.rb
@@ -5,7 +5,12 @@ class MessagesController < ApplicationController
 
   def create
   	@message = Message.new(params[:message].permit(:content, :username, :app_id))
-  	@message.save
+  	if @message.save
+  		flash[:notice] = "Message posted successfully"
+  		redirect_to message_path(@message)
+  	else
+  		flash[:error] = "Error posting message"
+  	end
 #  	unless (message[content] == nil) or (message[username] == nil) or (message[app_id] == nil)
 #  		@message = Message.new( :content => content, :username => username, :app_id => app_id, :active => true)
 #  	end
--- a2/messagehub/app/models/message.rb
+++ a2/messagehub/app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ActiveRecord::Base
 #	attr_accessible :content, :username, :app_id
+	validates_presence_of :content, :username, :app_id
 end

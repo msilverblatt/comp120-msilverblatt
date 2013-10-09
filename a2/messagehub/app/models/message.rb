80eae487c243fa9d7f5a0339d015727030a6e43a
msilverblatt <msilverblatt@gmail.com>
2013-10-09 11:44:20 -0400

    fix case where there are 0 messages

--- a2/messagehub/app/models/message.rb
+++ a2/messagehub/app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
 #	attr_accessible :content, :username, :app_id
 	validates_presence_of :content, :username, :app_id
+	validates :content, length: { maximum: 160}, allow_blank: false
+	validates :username, length: {minimum: 3, maximum: 60}, allow_blank: false
 	belongs_to :app
 	after_initialize :default_values
 
